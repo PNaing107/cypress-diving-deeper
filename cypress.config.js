fb97108ac5e5149163c0ef435763f76cf604e894
Phone Naing <phone.naing.dev@gmail.com>
2023-09-17 16:13:58 +0100

    mock a task for reference

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        seedDatabase() {
+          // Run your Node.js code
+        }
+      });
     },
   },
 });
