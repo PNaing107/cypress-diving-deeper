6c4c741a269c150fccd61e5b8d59145f4d770224
Phone Naing <phone.naing.dev@gmail.com>
2023-09-17 13:29:21 +0100

    test(contact form): form should submit if valid and highlight error if not

---
+++ cypress/e2e/contact.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+describe('contact form', () => {
+    it('should submit form', () => {
+        cy.visit('http://localhost:5173/about');
+        cy.get('[data-cy="contact-input-message"]').type('Hello world!');
+        cy.get('[data-cy="contact-input-name"]').type('John Doe');
+        cy.get('[data-cy="contact-input-email"]').type('test@example.com');
+        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
+        cy.get('[data-cy="contact-btn-submit"]').click();
+
+        // after submitting it should display loading state & button should also be disabled
+        cy.get('[data-cy="contact-btn-submit"]').contains('Sending...').and('have.attr', 'disabled');
+        // this is an alternative syntax
+        /*
+        cy.get('[data-cy="contact-btn-submit"]').then((el)) => {
+            expect(el.attr('disabled')).to.be.defined;
+            expect(el.text()).to.eq('Sending...');
+        }
+        */
+    });
+
+    it('should validate the form input', () => {
+        cy.visit('http://localhost:5173/about');
+        cy.get('[data-cy="contact-btn-submit"]').click();
+        cy.get('[data-cy="contact-btn-submit"]').then(el => {
+            expect(el).to.not.have.attr('disabled');
+            expect(el.text()).not.eq('Sending...');
+        });
+        // when the input field loses focus an invalid styling should be applied
+        cy.get('[data-cy="contact-input-message"]').focus().blur();
+        cy.get('[data-cy="contact-input-message"]').parent().should('have.attr', 'class').and('match', /invalid/);
+        cy.get('[data-cy="contact-input-name"]').focus().blur();
+        cy.get('[data-cy="contact-input-name"]').parent().should('have.attr', 'class').and('match', /invalid/); // this is another way of testing for the class attribute using regex and it doesn't fail when using the Cypress test runner
+    });
+});
